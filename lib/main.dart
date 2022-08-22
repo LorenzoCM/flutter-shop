1995268d238f3ea0241f12219e476f3a9ea562f7
LorenzoCM <lorenzojcastillom@gmail.com>
2022-08-22 19:59:01 -0400

    add ingredients to meal detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
-        theme: ThemeData(
-          colorScheme: Theme.of(context).colorScheme.copyWith(
-                primary: Colors.pink,
-                secondary: Colors.amber,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: Colors.pink,
+              secondary: Colors.amber,
+            ),
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
               ),
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                bodyText2: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                headline6: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold,
-                ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
               ),
-        ),
-        home: CategoriesScreen(),
-        routes: {
-          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        });
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => CategoriesScreen(),
+        );
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => CategoriesScreen(),
+        );
+      },
+    );
   }
 }
